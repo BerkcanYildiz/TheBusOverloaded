d7a60b50970a9d08d7e897badf3566c6458cf2c7
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-09-16 17:01:46 +0300

    Added LoadScene Script

---
+++ Assets/Scrips/ExampleScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    
+
+    /* private void Start()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(1 * 2.0f, 1, 1), Quaternion.identity);
+        }
+    }  public GameObject prefab;*/
+}
