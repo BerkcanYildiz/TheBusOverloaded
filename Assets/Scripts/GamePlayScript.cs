8d01770e36255a48de761c97eca46905f436914e
BerkcanYildiz <brkcnyldz66@gmail.com>
2022-09-22 15:53:59 +0300

    Updated GamePlay.cs

--- Assets/Scripts/GamePlayScript.cs
+++ Assets/Scripts/GamePlayScript.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,7 +6,9 @@ public class GamePlayScript : MonoBehaviour
 {
     public static int clickCounter = 0;
     public static int busCounter = 1;
-    [SerializeField] private static int levelPoints = 0;
+    public float decimalHealth;
+    
+    [SerializeField] private static float levelPoints = 0;
     [SerializeField] private GameObject gameObj;
     [SerializeField] private GameObject busPrefab;
     [SerializeField] private Text scoreText;
@@ -22,19 +25,53 @@ public class GamePlayScript : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                clickCounter++;
+                decimalHealth += 1 * Time.deltaTime;
+                clickCounter = Mathf.RoundToInt(decimalHealth);
                 Debug.Log(clickCounter);
-                if (clickCounter == 1000)
-                {
-                    Destroy(gameObj);
-                    busCounter++;
-                    levelPoints += clickCounter;
-                    scoreText.text = "SCORE : " + levelPoints;
-                    clickCounter = 0;
-                    Instantiate(busPrefab, new Vector3(0, 1, 1), Quaternion.identity);
-                }
             }
-            else
+            if (Input.GetMouseButtonUp(0) && clickCounter < 10)
+            {
+                levelPoints += 100;
+                Destroy(gameObj);
+                busCounter++;
+                scoreText.text = "SCORE : " + levelPoints;
+                clickCounter = 0;
+                decimalHealth = 0.0f;
+                Instantiate(busPrefab, new Vector3(0, 1, 1), Quaternion.identity);
+            }
+            else if (Input.GetMouseButtonUp(0) && clickCounter > 10)
+            {
+                levelPoints += 200;
+                Destroy(gameObj);
+                busCounter++;
+                scoreText.text = "SCORE : " + levelPoints;
+                clickCounter = 0;
+                Instantiate(busPrefab, new Vector3(0, 1, 1), Quaternion.identity);
+            }
+            else if (Input.GetMouseButtonUp(0) && clickCounter > 20)
+            {
+                levelPoints += 300;
+                Destroy(gameObj);
+                busCounter++;
+                scoreText.text = "SCORE : " + levelPoints;
+                clickCounter = 0;
+                Instantiate(busPrefab, new Vector3(0, 1, 1), Quaternion.identity);
+            }
+        }
+        else if(busCounter  == 6 && levelPoints == 1000 )
+        {
+            Debug.Log("Level completed successfully.");
+            Debug.Log(levelPoints); 
+        }
+        else
+        {
+            Debug.Log("Game Failed.");
+            Debug.Log(levelPoints);   
+        }
+    }
+}
+
+/* else
             {
                 clickCounter++;
                 Debug.Log(clickCounter);
@@ -47,12 +84,4 @@ public class GamePlayScript : MonoBehaviour
                     clickCounter = 0;
                     Instantiate(busPrefab, new Vector3(0, 1, 1), Quaternion.identity);
                 }
-            }
-        }
-        else
-        {
-            Debug.Log("Game Finished.");
-            Debug.Log(levelPoints);
-        }
-    }
-}
+            } */
